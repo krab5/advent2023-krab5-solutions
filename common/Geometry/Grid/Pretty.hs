0b5ed0891903d4ef073260c65c2c4eb1f63e0b0e
krab5 <crab.delicieux@gmail.com>
2023-12-13 11:35:24 +0100

    Updated module to work with SGR

--- common/Geometry/Grid/Pretty.hs
+++ common/Geometry/Grid/Pretty.hs
@@ -2,11 +2,11 @@ module Geometry.Grid.Pretty where
 
 import Geometry.Grid
 import Data.List (intercalate)
-import TermColor
+import qualified SGR
 
 -- | Pretty print the grid in a string with potential term colors (ANSI escape codes) depending on a set of
 -- predicates. Useful for highlighting some parts of the grid, typically.
-prettyMarkup :: (a -> Char) -> [((Int,Int) -> a -> Bool, TermColor)] -> Grid a -> String
+prettyMarkup :: (a -> Char) -> [((Int,Int) -> a -> Bool, [SGR.SGR])] -> Grid a -> String
 prettyMarkup showC styleIf g =
     intercalate "\n" $ map concat $ map (\ro -> map (\co -> mkc (ro,co)) [0..g_width g - 1]) [0..g_height g - 1]
     where mkc x =
@@ -14,7 +14,7 @@ prettyMarkup showC styleIf g =
                 let c = showC v in
                     case findTc x v styleIf of
                         Nothing -> [c]
-                        Just tc -> style tc [c]
+                        Just tc -> SGR.style tc [c]
           findTc x v [] = Nothing
           findTc x v ((p,t):ts)
             | p x v = Just t
